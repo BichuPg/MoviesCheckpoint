c5d5d91a806f05d7e38081c654f2c19efd240da8
BichuPg <115611237+BichuPg@users.noreply.github.com>
2023-09-29 09:51:23 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,97 +2,97 @@ class script(object):
     START_TXT = """Hello {},
 Myself <a href=https://t.me/{}>{}</a>,\n\nTrust me ! I can't even imagine how super-fast i can drive your Database channel \n\nAre you ready for Long Drive Baby...🤪"""
     LZTHMB_TEXT = """Hello {},
-Glad to see you here. It seems that you really love <a href=https://t.me/LazyDeveloperr >LazyDeveloper's</a> work.\n\n<b>Thumbnail extracting</b> feature will be available soon, please join <a href=https://t.me/LazyDeveloper>Dev Channel</a> and stay tuned for next <a href=https://t.me/LazyDeveloper>update</a>.\n\n  🐞 Report Bug here: <a href=http://t.me/LazyDeveloperSupport>LazyDev Support</a>
+✔️Glad to see you here. It seems that you really love <a href=https://t.me/PirateGamers>PirateGamers's</a> work.\n\n<b>Thumbnail extracting</b> feature will be available soon, please join <a href=https://t.me/Technical_Bots>Dev Channel</a> and stay tuned for next <a href=https://www.youtube.com/@MoviesCheckpoints>update</a>.\n\n  🐞 Report Bug here: <a href=http://t.me/Bugss_Support_Bot>Movies_Checkpoint_Group Support</a>
     """
     LZLINK_TEXT = """Hey {},
-Glad to see you here. It seems that you really love <a href=https://t.me/LazyDeveloperr >LazyDeveloper's</a> work.\n\n<b>File to LiNK converting</b> feature will be available soon, please join <a href=https://t.me/LazyDeveloper>Dev Channel</a> and stay tuned for next <a href=https://t.me/LazyDeveloper>update</a>.\n\n  🐞 Report Bug here: <a href=http://t.me/LazyDeveloperSupport>LazyDev Support</a>
+✔️Glad to see you here. It seems that you really love <a href=https://t.me/PirateGamers >PirateGamers</a> work.\n\n<b>File to LiNK converting</b> feature will be available soon, please join <a href=https://t.me/LazyDeveloper>Dev Channel</a> and stay tuned for next <a href=https://t.me/LazyDeveloper>update</a>.\n\n  🐞 Report Bug here: <a href=http://t.me/Bugss_Support_Bot>Bugs Support</a>
     """
     DNT_TEXT = """Hey sweetie {},
-Thanks for thinking about us.\nIt seems that you really love <a href=https://t.me/LazyDeveloperr >LazyDeveloper's</a> work.\n\n<b>For your kind information, we do not ask or force anyone for any kind of payment</b>. But if you really want to donate us then you can send money to us from below links...\n\n💵 Reach Donation Page : <a href=http://t.me/DonateLazyDeveloper>Click here...</a>\n\nT❤️ hank you so much..
+✔️Thanks for thinking about us.\nIt seems that you really love <a href=https://t.me/PirateGamers >PirateGamers</a> work.\n\n<b>For your kind information, we do not ask or force anyone for any kind of payment</b>. But if you really want to donate us then you can send money to us from below links...\n\n💵 Reach Donation Page : <a href=http://t.me/DonateLazyDeveloper>Click here...</a>\n\nT❤️ hank you so much..
     """
     REQ_AUTH_TEXT = """Hello {},
-\nSorry sweetie.. You must have to be the Authentic User to complete this operation...\n\n👮‍♀ REPORT ISSUE HERE: <a href=https://t.me/LazyDeveloperSupport>LazyDeveloper Support</a>\n\n
+\nSorry sweetie.. You must have to be the Authentic User to complete this operation...\n\n👮‍♀ REPORT ISSUE HERE: <a href=http://t.me/Bugss_Support_Bot>Bugs Support</a>\n\n
     """
     ALRDY_UPLDD_TEXT = """✅ Content is already uploaded.\n\nName:{}\nPlease make sure about your spelling before submiting request..."""
     HELP_TXT = """𝙷𝙴𝚈 {}
 Here is the help for my COMMANDS."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/LazyDeveloper>LazyDeveloper</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-    SOURCE_TXT = """<b>NOTE:</b>
-- Lazy Princess is an open source project. 
-- Source - https://github.com/LazyDeveloperr/LazyPrincess  
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/PirateGamers>⚡️PirateGamers⚡️</a>
+✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 🔥𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼🔥
+✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: ⚡️𝙿𝚈𝚃𝙷𝙾𝙽 𝟹⚡️
+✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 🏆𝙼𝙾𝙽𝙶𝙾 𝙳𝙱🏆
+✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 🍿𝙷𝙴𝚁𝙾𝙺𝚄🍿
+✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: ✅️v1.0.1 [ 𝙱𝙴𝚃𝙰 ]✅️"""
+    SOURCE_TXT = """<b>⚡️NOTE⚡️:</b>
+✔️ This Bot is Not an open source project. 
+✔️ Contact - http://t.me/Bugss_Support_Bot  
 
-<b>DEVS:</b>
-- <a href=https://t.me/LazyDeveloper>LazyDeveloper</a>"""
+<b>🔥DEVS🔥:</b>
+✔️ <a href=https://t.me/PirateGamers>⚡️PirateGamers⚡️</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and LazyPriness will respond whenever that keyword hits the message
+- Filter is the feature were users can set automated replies for a particular keyword and bot will respond whenever that keyword hits the message
 
-<b>NOTE:</b>
+<b>🔥NOTE🔥:</b>
 1. BOT should have admin privillage.
 2. Only admins can add filters in a chat.
 3. Alert buttons have a limit of 64 characters.
 
-<b>Commands and Usage:</b>
-• /filter - <code>add a filter in chat</code>
-• /filters - <code>list all the filters of a chat</code>
-• /del - <code>delete a specific filter in chat</code>
-• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
-    BUTTON_TXT = """Help: <b>Buttons</b>
+<b>✨️Commands and Usage✨️:</b>
+▪️ /filter - <code>add a filter in chat</code>
+▪️ /filters - <code>list all the filters of a chat</code>
+▪️ /del - <code>delete a specific filter in chat</code>
+▪️ /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
+    BUTTON_TXT = """Help: <b>⚡️Buttons⚡️</b>
 
 - Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. BOT supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format
+🎗 Telegram will not allows you to send buttons without any content, so content is mandatory.
+🎗 BOT supports buttons with any telegram media type.
+🎗 Buttons should be properly parsed as markdown format
 
-<b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/LazyDeveloper)</code>
+<b>🍿URL buttons🍿:</b>
+<code>[Button Text](buttonurl:https://t.me/telegram)</code>
 
-<b>Alert buttons:</b>
+<b>💌Alert buttons💌:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
-    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
+    AUTOFILTER_TXT = """Help: <b>🔮Auto Filter🔮</b>
 
-<b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
-2. Make sure that your channel does not contains camrips, porn and fake files.
-3. Forward the last message to me with quotes.
+<b>✔️NOTE✔️:</b>
+🏆 Make me the admin of your channel if it's private.
+🏆 Make sure that your channel does not contains camrips, porn and fake files.
+🏆 Forward the last message to me with quotes.
  I'll add all the files in that channel to my db."""
-    CONNECTION_TXT = """Help: <b>Connections</b>
+    CONNECTION_TXT = """Help: <b>🥇Connections🥇</b>
 
-- Used to connect bot to PM for managing filters 
-- it helps to avoid spamming in groups.
+➡️ Used to connect bot to PM for managing filters 
+➡️ it helps to avoid spamming in groups.
 
-<b>NOTE:</b>
-1. Only admins can add a connection.
-2. Send <code>/connect</code> for connecting me to ur PM
+<b>🌐NOTE🌐:</b>
+🎞 Only admins can add a connection.
+🎞 Send <code>/connect</code> for connecting me to ur PM
 
-<b>Commands and Usage:</b>
-• /connect  - <code>connect a particular chat to your PM</code>
-• /disconnect  - <code>disconnect from a chat</code>
-• /connections - <code>list all your connections</code>"""
+<b>🔥Commands and Usage🔥:</b>
+✔️ /connect  - <code>connect a particular chat to your PM</code>
+✔️ /disconnect  - <code>disconnect from a chat</code>
+✔️ /connections - <code>list all your connections</code>"""
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
-<b>NOTE:</b>
+<b>🎗NOTE🎗:</b>
 these are the extra features of Lazy Princess
 
 <b>Commands and Usage:</b>
-• /id - <code>get id of a specified user.</code>
-• /info  - <code>get information about a user.</code>
-• /imdb  - <code>get the film information from IMDb source.</code>
-• /search  - <code>get the film information from various sources.</code>"""
-    ADMIN_TXT = """Help: <b>Admin mods</b>
+● /id - <code>get id of a specified user.</code>
+○ /info  - <code>get information about a user.</code>
+● /imdb  - <code>get the film information from IMDb source.</code>
+○ /search  - <code>get the film information from various sources.</code>"""
+    ADMIN_TXT = """Help: <b>📁Admin mods📁</b>
 
 <b>NOTE:</b>
-This module only works for my admins
+📢This module only works for my admins📢
 
-<b>Commands and Usage:</b>
+<b>C📧ommands and Usage📧:</b>
 • /logs - <code>to get the rescent errors</code>
 • /stats - <code>to get status of files in db.</code>
 • /delete - <code>to delete a specific file from db.</code>
@@ -105,10 +105,10 @@ This module only works for my admins
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
     STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>🔥
+★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>🔥
+★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱🔥
+★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱🔥"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
