b23cbd9f0ed255a845e6ecb66f39dd55253e6905
BichuPg <115611237+BichuPg@users.noreply.github.com>
2023-09-29 10:13:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -21,19 +21,17 @@ BATCH_FILES = {}
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        buttons = [
-            [
-                InlineKeyboardButton('🔔 Updates 🤖', url='https://t.me/LazyDeveloper')
-            ],
-            [
-                InlineKeyboardButton('🙆🏻 Help 🦾', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            ],[
-            InlineKeyboardButton('Watch Tutorial', url='https://youtube.com/LazyDeveloper')
-            ],
-            [
-                InlineKeyboardButton(text=DOWNLOAD_TEXT_NAME,url=DOWNLOAD_TEXT_URL)
-            ]
-            ]
+        buttons = [[
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+        ], [
+            InlineKeyboardButton('🔥Whatsapp🔥', url='https://whatsapp.com/channel/0029VaAUhsE0LKZ8uFkfw03a'),
+            InlineKeyboardButton('🔔 Updates 🔔', url='https://t.me/PirateGamers')
+        ], [
+            InlineKeyboardButton('🏆 Help 🏆', callback_data='help'),
+            InlineKeyboardButton('🍿 About 🍿', callback_data='about')
+        ],[
+            InlineKeyboardButton('🌐Watch Tutorial🌐', url='https://t.me/+AKANipq4DHhkYjk9')
+        ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
         await asyncio.sleep(2) # 😢 https://github.com/LazyDeveloperr/LazyPrincess/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
@@ -47,16 +45,16 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕↖️ Add Me To Your Groups ↗️➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🧞‍♀️ Search 🧐', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🔔 Updates 🤖', url='https://t.me/LazyDeveloper')
-            ],[
-            InlineKeyboardButton('🙆🏻 Help 🦾', callback_data='help'),
-            InlineKeyboardButton('♥️ About ♥️', callback_data='about')
-            ],[
-            InlineKeyboardButton('Watch Tutorial', url='https://youtube.com/LazyDeveloper')
-            ]]
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+        ], [
+            InlineKeyboardButton('🔥Whatsapp🔥', url='https://whatsapp.com/channel/0029VaAUhsE0LKZ8uFkfw03a'),
+            InlineKeyboardButton('🔔 Updates 🔔', url='https://t.me/PirateGamers')
+        ], [
+            InlineKeyboardButton('🏆 Help 🏆', callback_data='help'),
+            InlineKeyboardButton('🍿 About 🍿', callback_data='about')
+        ],[
+            InlineKeyboardButton('🌐Watch Tutorial🌐', url='https://t.me/+AKANipq4DHhkYjk9')
+        ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
@@ -79,7 +77,7 @@ async def start(client, message):
             ],
              [
                 InlineKeyboardButton(
-                    "🦋 SUBSCRIBE YT Channel 🦋", url='https://youtube.com/@LazyDeveloperr'
+                    "🦋 SUBSCRIBE YT Channel 🦋", url='https://www.youtube.com/@MoviesCheckpoints'
                 )
             ]
         ]
@@ -100,15 +98,15 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕↖️ Add Me To Your Groups ↗️➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🧞‍♀️ Search 🧐', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🔔 Updates 🤖', url='https://t.me/LazyDeveloper')
-            ],[
-            InlineKeyboardButton('🙆🏻 Help 🦾', callback_data='help'),
-            InlineKeyboardButton('♥️ About ♥️', callback_data='about')
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+        ], [
+            InlineKeyboardButton('🔥Whatsapp🔥', url='https://whatsapp.com/channel/0029VaAUhsE0LKZ8uFkfw03a'),
+            InlineKeyboardButton('🔔 Updates 🔔', url='https://t.me/PirateGamers')
+        ], [
+            InlineKeyboardButton('🏆 Help 🏆', callback_data='help'),
+            InlineKeyboardButton('🍿 About 🍿', callback_data='about')
         ],[
-            InlineKeyboardButton('Watch Tutorial', url='https://youtube.com/LazyDeveloper')
+            InlineKeyboardButton('🌐Watch Tutorial🌐', url='https://t.me/+AKANipq4DHhkYjk9')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -384,7 +382,7 @@ async def delete_all_index(bot, message):
 @Client.on_callback_query(filters.regex(r'^autofilter_delete'))
 async def delete_all_index_confirm(bot, message):
     await Media.collection.drop()
-    await message.answer('♥️ Thank You LazyDeveloper ♥️')
+    await message.answer('♥️ Thank You PirateGamers ♥️')
     await message.message.edit('Succesfully Deleted All The Indexed Files.')
 
 
